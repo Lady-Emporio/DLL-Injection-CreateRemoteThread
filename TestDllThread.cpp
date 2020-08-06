7decc909f0fb053f5811cce53aacc83706b1a1f4
Lady-Emporio <qweqwe6664@gmail.com>
2020-08-06 16:47:18 +0300

    Add files via upload

--- TestDllThread.cpp
+++ TestDllThread.cpp
@@ -1,11 +1,10 @@
-﻿// TestDllThread.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿
 #include <Windows.h>
 #include <strsafe.h>
 #include <iostream>
 
 //#define DLL_NAME "C:\\Users\\Иванова\\source\\repos\\Dll1\\Debug\\Dll1.dll"
-#define DLL_NAME "C:\\Users\\Иванова\\source\\repos\\Dll1\\x64\\Release\\Dll1.dll"
+#define DLL_NAME "C:/Users/Иванова/source/repos/HookWithDll/x64/Release/HookWithDll.dll"
 
 BOOL InjectRemoteThread(DWORD ProcessID)
 {
@@ -41,7 +40,7 @@ BOOL InjectRemoteThread(DWORD ProcessID)
     return 1;
 }
 
-
+/*
 void t1() {
 
     const char* buffer = "C:\\Users\\Иванова\\source\\repos\\Dll1\\x64\\Release\\Dll1.dll";
@@ -81,12 +80,45 @@ void t1() {
 
     CloseHandle(process);
 }
+
+*/
 int main()
 {
-    bool res = InjectRemoteThread(6300);
-    std::cout << res << std::endl;
+    const char* buffer = "C:\\Users\\Иванова\\source\\repos\\Dll2\\x64\\Release\\Dll2.dll";
 
-    //t1();
+    int procID = 9300;
+    HANDLE process = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procID);
+    if (process == NULL) {
+        printf("Error: the specified process couldn't be found.\n");
+    }
 
-    
+
+    LPVOID addr = (LPVOID)GetProcAddress(GetModuleHandle(L"kernel32.dll"), "LoadLibraryA");
+    if (addr == NULL) {
+        printf("Error: the LoadLibraryA function was not found inside kernel32.dll library.\n");
+    }
+
+
+    LPVOID arg = (LPVOID)VirtualAllocEx(process, NULL, strlen(buffer), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+    if (arg == NULL) {
+        printf("Error: the memory could not be allocated inside the chosen process.\n");
+    }
+
+
+    int n = WriteProcessMemory(process, arg, buffer, strlen(buffer), NULL);
+    if (n == 0) {
+        printf("Error: there was no bytes written to the process's address space.\n");
+    }
+
+
+    HANDLE threadID = CreateRemoteThread(process, NULL, 0, (LPTHREAD_START_ROUTINE)addr, arg, NULL, NULL);
+    if (threadID == NULL) {
+        printf("Error: the remote thread could not be created.\n");
+    }
+    else {
+        printf("Success: the remote thread was successfully created.\n");
+    }
+
+    CloseHandle(process);
+
 }
